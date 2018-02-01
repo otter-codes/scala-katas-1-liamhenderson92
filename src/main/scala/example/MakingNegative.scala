e6e0b71ba3393be90349834851e06d1eed09417a
Liam Henderson <liam.henderson@digital.hmrc.gov.uk>
2018-02-01 17:47:25 +0000

    add negative numbers

--- src/main/scala/example/MakingNegative.scala
+++ src/main/scala/example/MakingNegative.scala
@@ -1,7 +1,23 @@
 package example
 
-object MakingNegative {
+import scala.io.StdIn
 
+import scala.math.abs
 
+object MakingNegative extends App {
 
+  val numberInput : Int = StdIn.readLine("Enter your number: ").toInt
+
+  
+
+//  val newNumber : Int
+
+  if(numberInput > 0){
+
+//    (numberInput*-1 = newNumber)
+
+    println("-" + numberInput)
+  }
+  else
+    println(numberInput)
 }
