d193d8a24b5f74b883f41cda24e97e171314f2ba
Liam Henderson <liam.henderson@digital.hmrc.gov.uk>
2018-02-02 10:10:36 +0000

    change negative to function

--- src/main/scala/example/MakingNegative.scala
+++ src/main/scala/example/MakingNegative.scala
@@ -7,12 +7,15 @@ import scala.math.abs
 object MakingNegative extends App {
 
   val numberInput : Int = StdIn.readLine("Enter your number: ").toInt
+  val newNumber = makeNegative(numberInput)
 
+    println(newNumber)
 
-  if(numberInput > 0){
+  def makeNegative(newNumber : Int) = {
+    if (newNumber > 0){
+      (newNumber * -1)
+    }else newNumber
 
-    println((numberInput * -1))
   }
-  else
-    println(numberInput)
+
 }
