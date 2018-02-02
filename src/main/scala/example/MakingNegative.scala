6e881ffb9f109a9f31d0eafd3077821dbd0d97fd
Liam Henderson <liam.henderson@digital.hmrc.gov.uk>
2018-02-02 09:10:31 +0000

    update negative numbers

--- src/main/scala/example/MakingNegative.scala
+++ src/main/scala/example/MakingNegative.scala
@@ -8,15 +8,10 @@ object MakingNegative extends App {
 
   val numberInput : Int = StdIn.readLine("Enter your number: ").toInt
 
-  
-
-//  val newNumber : Int
 
   if(numberInput > 0){
 
-//    (numberInput*-1 = newNumber)
-
-    println("-" + numberInput)
+    println((numberInput * -1))
   }
   else
     println(numberInput)
